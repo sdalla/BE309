ddb629b57c33832fb7b71c8f5b92336d674f0858
Shiv Dalla <sdalla@seas.upenn.edu>
2017-11-11 16:57:48 -0500

    heat map stuff

--- heatmapping.m
+++ heatmapping.m
@@ -1,2 +1,11 @@
-file = 'data analysis.xlsx';
-data = readtable(file);
+file = 'ext:cont copy.csv';
+data = csvread(file);
+
+data(data == -99999) = NaN;
+xvars = data(1,2:end);
+
+cmap = colormap(spring);
+h = heatmap(xvars, 1:5, data(2:end,2:end));
+xlabel('Frequency (Hz)')
+ylabel('Leg')
+title('Leg Angle Response to 1Vpp Stimulation')
